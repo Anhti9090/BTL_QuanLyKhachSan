d3a7d3e3d594b994a1bdb1760d49bef6691f4ad1
Cuong <cuong25b@yahoo.com>
2026-09-17 23:10:26 +0700

    test commit

---
+++ BTL_QuanLyKhachSan/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
